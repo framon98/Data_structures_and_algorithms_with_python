cc864461ad728f79066bf2548e444623528c8017
Francisco Ramón Castro García <framon.98@outlook.com>
2024-07-19 21:26:13 -0600

    Add comment to explain function

--- Chapter 3/list_accumulator.py
+++ Chapter 3/list_accumulator.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def revList(lst):
+    """
+    This function uses the accumulator pattern
+    to reverse the list. This is not as efficient
+    as using a recusrive approach. This uses more
+    cpu cycles since it has to read the whole list
+    and add it to a new copy
+    """
     accumulator = []
 
     for idx in lst:
