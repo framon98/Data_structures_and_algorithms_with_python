036dd540c37af6dfb6df4dc7e9e90d0b7e1a2b28
Francisco Ramón Castro García <framon.98@outlook.com>
2024-08-08 22:25:27 -0600

    add getter and setter function

--- Chapter 4/PyList.py
+++ Chapter 4/PyList.py
@@ -13,4 +13,15 @@ class PyList:
         for element in contents:
             self.append(element)
 
-    
+    def __getitem__(self, index):
+        if index >= 0 and index < self.numItems:
+            return self.items(index)
+
+        raise IndexError("PyList index out of range")
+
+    def __setitem__(self, index, val):
+        if index >= 0 and index < self.numItems:
+            self.items[index] = val
+            return
+
+        raise IndexError("PyList assignment index out of range")
