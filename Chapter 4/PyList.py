842d1073dbe3524b43499bf1f2033b1bfed7f118
Francisco Ramón Castro García <framon.98@outlook.com>
2024-08-08 22:20:25 -0600

    Add coinstructor steps

--- Chapter 4/PyList.py
+++ Chapter 4/PyList.py
@@ -6,6 +6,9 @@ class PyList:
         contents: allows to create a list with variables initial contents
         initial_size: allows the progrmmer to pick a size for the internal size of the list.
         """
+        self.items = [None] * size
+        self.numItems = 0
+        self.size = size
 
         for element in contents:
             self.append(element)
